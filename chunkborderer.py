c5612b5362d27b5b733c7e6fe1731250588d9d84
lahwran <blendmaster1024@gmail.com>
2011-05-29 00:44:09 -0600

    init

---
+++ chunkborderer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import sys, os
+sys.path.add(os.realpath(".."))
+
+#import pymclevel
+try:
+    from pymclevel import mclevel
+    err=False
+except ImportError:
+    err=True
+if err:
+    try:
+        import mclevel
+        err=False
+    except ImportError:
+        err=True
+if err:
+    print "this script requires pymclevel to be on the pythonpath."
+    print "get it from https://github.com/codewarrior0/pymclevel"
+    print "or use one of these:"
+    print "$ wget --no-check-certificate https://github.com/codewarrior0/pymclevel/tarball/master && tar -xf codewarrior0-pymclevel-*.tar.gz"
+    print "$ git clone https://github.com/codewarrior0/pymclevel.git"
+    sys.exit(1)
+    
+#get the world and a chunk set
+world = mclevel.fromFile("world")
+chunkPositions = set(world.allChunks)
+
+def vadd(vec1, vec2):
+    if len(vec1) != len(vec2):
+        raise Exception("mismatched lengths")
+    return tuple([vec1[i]+vec2[i] for i in range(len(vec1))])
+    
+#offset definitions
+square=((1,0),(-1,0),(0,1),(0,-1))
+square3=[(15,16,0,16,0,128), (0,1,0,16,0,128), (0,16,15,16,0,128), (0,16,0,1,0,128)]
+
+#find the edges
+allwalls = [(c,tuple([(vadd(c, ofs) not in chunkPositions) for ofs in square])) for c in chunkPositions]
+walls=[x for x in walls if sum(x[1]) > 0]
+
+#add bedrock to the edges
+for wall in walls:
+    c = world.getChunk(wall[0][0], wall[0][1])
+    for num in range(4):
+        if wall[1][num]:
+            i=square3[num]
+            c.Blocks[i[0]:i[1],i[2]:i[3],i[4]:i[5]] = 7
+    c.chunkChanged()
+    
+#save
+world.saveInPlace()
